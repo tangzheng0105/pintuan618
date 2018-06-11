5f26b6e9ef43a82e93798c9aa0b768ac1beb8ecb
tangzheng <tangzheng0105@gmail.com>
2018-06-11 13:16:15 +0800

    增加图片懒加载

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,7 +1,7 @@
 function GetQueryString(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
     var r = window.location.search.substr(1).match(reg);
-    if (r != null) return decodeURI(r[2]); return null;
+    if (r != null) return decodeURIComponent(r[2]); return null;
 };
 
 /**
